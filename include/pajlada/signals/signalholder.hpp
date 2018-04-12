3f6645c615ff7bf412c05fe322e589cbdd34ff9b
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-04-12 21:02:11 +0200

    Add SignalHolder class that you can inherit from and use managedConnect instead

---
+++ include/pajlada/signals/signalholder.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <pajlada/signals/connection.hpp>
+
+#include <vector>
+
+namespace pajlada {
+namespace Signals {
+
+class SignalHolder
+{
+    std::vector<ScopedConnection> _managedConnections;
+
+public:
+    virtual ~SignalHolder() = default;
+    SignalHolder() = default;
+
+    // Make non-copyable. Not sure if this is required
+    SignalHolder(const SignalHolder &other) = delete;
+    SignalHolder &operator=(const SignalHolder &other) = delete;
+
+    template <typename Signal, typename Callback>
+    void
+    managedConnect(Signal &signal, Callback cb)
+    {
+        this->_managedConnections.emplace_back(signal.connect(cb));
+    }
+};
+
+}  // namespace Signals
+}  // namespace pajlada
