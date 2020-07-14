484c50caa11ab1ea25c38caff7efe42a91fb6a6d
Cranken <cranken1337@gmail.com>
2020-07-14 21:50:36 +0200

    Add self disconnecting signal
    This signal type disconnects itself when the callback function returns true
    
    Add test and fix error.
    
    Fix an error which did not happen in msvc only on gcc.

--- src/main.cpp
+++ src/main.cpp
@@ -307,3 +307,36 @@ TEST(ScopedConnection, STLContainer)
     incrementSignal.invoke(1);
     EXPECT_EQ(a, 2);
 }
+
+TEST(SelfDisconnectingSignal, MultipleConnects)
+{
+    NoArgSelfDisconnectingSignal signal;
+
+    int a = 0;
+    int b = 0;
+
+    signal.connect([&] {
+        a++;
+        return a == 1;
+    });
+
+    signal.connect([&] {
+        b++;
+        return b == 2;
+    });
+
+    signal.invoke();
+
+    EXPECT_EQ(a, 1);
+    EXPECT_EQ(b, 1);
+
+    signal.invoke();
+
+    EXPECT_EQ(a, 1);
+    EXPECT_EQ(b, 2);
+
+    signal.invoke();
+
+    EXPECT_EQ(a, 1);
+    EXPECT_EQ(b, 2);
+}
