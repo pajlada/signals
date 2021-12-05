05e0e06e4c8593aaff172a9bd62a9f5c5e3a645d
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-12-05 14:43:46 +0100

    Add signal-holder tests

---
+++ include/pajlada/signals.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <pajlada/signals/connection.hpp>
+#include <pajlada/signals/scoped-connection.hpp>
+#include <pajlada/signals/signal.hpp>
+#include <pajlada/signals/signalholder.hpp>
