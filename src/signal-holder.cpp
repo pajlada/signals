c0e0c5b8c9ae713741f066d6b15d9a67a7993738
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2025-08-23 14:46:01 +0200

    test(SignalHolder): addConnection/emplace_back

--- src/signal-holder.cpp
+++ src/signal-holder.cpp
@@ -2,11 +2,37 @@
 
 #include <gtest/gtest.h>
 
-#include <vector>
-
 using namespace pajlada::Signals;
 
-TEST(SignalHolder, A)
+TEST(SignalHolder, AddConnection)
+{
+    Signal<int> incrementSignal;
+    int a = 0;
+    auto IncrementA = [&a](int incrementBy) {
+        a += incrementBy;  //
+    };
+    EXPECT_EQ(a, 0);
+
+    {
+        SignalHolder holder;
+
+        holder.addConnection(incrementSignal.connect(IncrementA));
+
+        incrementSignal.invoke(1);
+        EXPECT_EQ(a, 1);
+
+        Connection conn = incrementSignal.connect(IncrementA);
+        holder.addConnection(std::move(conn));
+
+        incrementSignal.invoke(1);
+        EXPECT_EQ(a, 3);
+    }
+
+    incrementSignal.invoke(1);
+    EXPECT_EQ(a, 3);
+}
+
+TEST(SignalHolder, ManagedConnect)
 {
     Signal<int> incrementSignal;
     int a = 0;
@@ -24,6 +50,36 @@ TEST(SignalHolder, A)
         EXPECT_EQ(a, 1);
 
         holder.managedConnect(incrementSignal, IncrementA);
+
+        incrementSignal.invoke(1);
+        EXPECT_EQ(a, 3);
+    }
+
+    incrementSignal.invoke(1);
+    EXPECT_EQ(a, 3);
+}
+
+TEST(SignalHolder, EmplaceBack)
+{
+    Signal<int> incrementSignal;
+    int a = 0;
+    auto IncrementA = [&a](int incrementBy) {
+        a += incrementBy;  //
+    };
+    EXPECT_EQ(a, 0);
+
+    {
+        SignalHolder holder;
+
+        holder.emplace_back(std::make_unique<ScopedConnection>(
+            incrementSignal.connect(IncrementA)));
+
+        incrementSignal.invoke(1);
+        EXPECT_EQ(a, 1);
+
+        Connection conn = incrementSignal.connect(IncrementA);
+        holder.emplace_back(
+            std::make_unique<ScopedConnection>(std::move(conn)));
 
         incrementSignal.invoke(1);
         EXPECT_EQ(a, 3);
